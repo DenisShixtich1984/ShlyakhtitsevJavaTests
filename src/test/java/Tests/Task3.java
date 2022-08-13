17ed70ba539708c9a2bd4c501d2de2f6192862a2
Den <skrizhali2001@gmail.com>
2022-08-13 14:31:29 +0300

    tests

--- src/test/java/Tests/Task3.java
+++ src/test/java/Tests/Task3.java
@@ -6,26 +6,18 @@ public class Task3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int count = 0;
-        int[] number = new int[3];
+        int[] number = new int[10];
         for (int i = 0; i < number.length; i++) {
-            System.out.println("Введите цифры для элемента с индексом :" + i + " :");
+            System.out.println("Введите цифры для элемента с индексом " + i + " :");
             number[i] = scanner.nextInt();
-
+        }
+        for (int i = 0; i < number.length; i++) {
             if (number[i] % 3 == 0) {
-                System.out.println("Число кратное трём : " + number[i]);
                 count++;
+                System.out.print(number[i] + " ");
             }
         }
-//        if (number[0] % 3 == 0) {
-//            System.out.println(number[0]);
-//        }
-//        if (number[1] % 3 == 0) {
-//            System.out.println(number[1]);
-//        }
-//        if (number[2] % 3 == 0) {
-//            System.out.println(number[2]);
-//        }
-
+        System.out.println("\n");
         System.out.println("Количество введенных цифр кратных 3-м равно: " + count);
     }
 }
--- src/test/java/Tests/Test1.java
+++ src/test/java/Tests/Test1.java
@@ -1,14 +1,23 @@
 package Tests;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * Для полноты алгоритма добавил вывод информации в консоль, если цифра равна или меньше (7)семи,
+ * а также обработку исключения при вводе буквенных символов
+ */
 public class Test1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите цифру :");
-        int i = scanner.nextInt();
-        if (i > 7) {
-            System.out.println("Привет");
-        } else System.out.println("Вы ввели число 7 или число меньше 7");
+        try {
+            int i = scanner.nextInt();
+            if (i > 7) {
+                System.out.println("Привет");
+            } else System.out.println("Вы ввели число 7 или число меньше 7");
+        } catch (InputMismatchException e) {
+            System.out.println("Данные ведены не верно");
+        }
     }
 }
