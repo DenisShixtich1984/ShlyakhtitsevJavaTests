e4505c216a572618e479c2df917b72df57d1bafb
Den <skrizhali2001@gmail.com>
2022-08-13 20:01:58 +0300

    final tests

--- src/test/java/Tests/Task3.java
+++ src/test/java/Tests/Task3.java
@@ -1,7 +1,9 @@
 package Tests;
 
 import java.util.Scanner;
-
+/**
+ * Для полноты алгоритма добавил вывод колличества цифр кратных 3-м в консоль
+ */
 public class Task3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
